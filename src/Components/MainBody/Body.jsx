6bbcf7a0a48317c954ddd959008d349defc76606
aryaun xames <ariankhamesi@gmail.com>
2024-02-24 14:20:32 +0330

    craete third part of body works component

--- src/Components/MainBody/Body.jsx
+++ src/Components/MainBody/Body.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Introduction from '../Introduction/Introduction'
 import Services from '../Services/Services'
+import Works from '../Works/Works'
 
 export default function Body() {
   return (
     <>
     <Introduction/>
     <Services/>
+    <Works/>
     </>
   )
 }
---
+++ src/Components/Works/Works.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export default function Works() {
+    return (
+        <div class="w-100 fourth-bg">
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col-12">
+                        <h3 class="text-white fs-1 custom-font">My Works</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="text-center">
+                            <div>
+                                <img src="images/work1.webp" class="img-fluid" alt="Mobile" />
+                            </div>
+                            <div class="mt-2 py-4 mb-70">
+                                <span class="text-white custom-font">Mobile App</span><br />
+                                <a href="#" class="text-decoration-none fs-3 color custom-font">Colorlib App Project</a>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <div>
+                                <div>
+                                    <img src="images/work2.webp" class="img-fluid" alt="Mobile" />
+                                </div>
+                                <div class="mt-2 py-4 mb-70">
+                                    <span class="text-white custom-font">Mobile App</span><br />
+                                    <a href="#" class="text-decoration-none fs-3 color custom-font">Colorlib App Project</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text-center">
+                            <div>
+                                <img src="images/work3.webp" class="img-fluid" alt="Mobile" />
+                            </div>
+                            <div class="mt-2 py-4 mb-70">
+                                <span class="text-white custom-font">Mobile App</span><br />
+                                <a href="#" class="text-decoration-none fs-3 color custom-font">Colorlib App Project</a>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <div>
+                                <div>
+                                    <img src="images/work4.webp" class="img-fluid" alt="Mobile" />
+                                </div>
+                                <div class="mt-2 py-4 mb-70">
+                                    <span class="text-white custom-font">Mobile App</span><br />
+                                    <a href="#" class="text-decoration-none fs-3 color custom-font">Colorlib App Project</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="custom-border2">
+                            <a href="#" class="w-100 h-100 d-block text-center text-decoration-none py-3 px-5 color more-work custom-font">More Works</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
