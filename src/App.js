d21f75055c31a25efebbe3f4b6587e78094f1b7d
aryaun xames <ariankhamesi@gmail.com>
2024-02-24 14:53:33 +0330

    set footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from "./Components/Header/Header"
 import Body from './Components/MainBody/Body';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <>
     <Header/>
     <Body/>
+    <Footer/>
     </>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div class="w-100 sixth-bg">
+            <div class="container">
+                <div class="row m-90">
+                    <div class="col-lg-8">
+                        <h3 class="text-white fs-1 mb-4 custom-font">Do you have any Project? <a href="#" class="text-decoration-none color custom-font">Let’s Talk</a></h3>
+                        <p class="text-white custom-font">Sed eleifend sed nibh nec fringilla. Donec eu cursus sem, vitae tristique ante. Cras pretium rutrum egestas. Integer ultrices libero sed justo vehicula, eget tincidunt tortor tempus.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-3">
+                        <div class="py-4 d-flex justify-content-between align-items-center px-custom custom-border icon-cell">
+                            <span class="text-white custom-span custom-font">Behance</span>
+                            <a href="#"><i class="bi bi-behance icon"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-3">
+                        <div class="py-4 d-flex justify-content-between align-items-center px-custom custom-border icon-cell">
+                            <span class="text-white custom-span custom-font">Dribble</span>
+                            <a href="#"><i class="bi bi-dribbble icon"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-3">
+                        <div class="py-4 d-flex justify-content-between align-items-center px-custom custom-border icon-cell">
+                            <span class="text-white custom-span custom-font">Medium</span>
+                            <a href="#"><i class="bi bi-medium icon"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-3">
+                        <div class="py-4 d-flex justify-content-between align-items-center px-custom custom-border icon-cell">
+                            <span class="text-white custom-span custom-font">Linkedin</span>
+                            <a href="#"><i class="bi bi-linkedin icon"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-12">
+                        <p class="text-center copy custom-font">Copyright ©2022 All rights reserved | This template is made with by <a href="https://colorlib.com/" class="text-decoration-none color custom-font">Colorlib</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
