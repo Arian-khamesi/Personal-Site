65afb1cb81da8180b50fac3071ea3944606720cd
aryaun xames <ariankhamesi@gmail.com>
2024-02-24 14:10:11 +0330

    set second part of body services

--- src/Components/MainBody/Body.jsx
+++ src/Components/MainBody/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Introduction from '../Introduction/Introduction'
+import Services from '../Services/Services'
 
 export default function Body() {
   return (
     <>
     <Introduction/>
+    <Services/>
     </>
   )
 }
---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Services() {
+    return (
+        <div class="w-100 third-bg">
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col-12">
+                        <h3 class="text-white custom-font">My Services</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-4 px-3">
+                        <div class="py-5 px-4 mx-auto text-center custom-border max-w">
+                            <img src="images/1.svg" alt="icon" />
+                            <h3 class="fs-4 mt-4 mb-3 color custom-font">Web & Mobile Design</h3>
+                            <p class="text-white custom-font">Sed eleifend sed nibh nec fringilla. Donec eu cursus sem vitae tristique ante ibero</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 px-3">
+                        <div class="py-5 px-4 mx-auto text-center custom-border max-w">
+                            <img src="images/2.svg" alt="icon" />
+                            <h3 class="fs-4 mt-4 mb-3 color custom-font">Web Development</h3>
+                            <p class="text-white custom-font">Sed eleifend sed nibh nec fringilla. Donec eu cursus sem vitae tristique ante ibero</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 px-3">
+                        <div class="py-5 px-4 mx-auto text-center custom-border max-w">
+                            <img src="images/3.svg" alt="icon" />
+                            <h3 class="fs-4 mt-4 mb-3 color custom-font">E-commerce</h3>
+                            <p class="text-white custom-font">Sed eleifend sed nibh nec fringilla. Donec eu cursus sem vitae tristique ante ibero</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
